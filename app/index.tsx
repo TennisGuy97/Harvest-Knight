e1c625e7872dcedc0dab79934d6175d0701520e6
TennisGuy97 <jervierhout@gmail.com>
2026-01-19 14:11:36 -0500

    Removing boiler plate, installed Expo video library

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+
+export default function Index() {
+  return <View></View>;
+}
+
+/*     <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View> */
