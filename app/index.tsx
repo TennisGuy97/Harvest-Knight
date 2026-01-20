3461d0c34f69308e2aa52de6cc9eef92c8d93afa
TennisGuy97 <jervierhout@gmail.com>
2026-01-19 19:25:56 -0500

    Added video background (not formatted properly to new aspect ratio of phones yet)

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,35 @@
-import { View } from "react-native";
+import { VideoView, useVideoPlayer } from "expo-video";
+import { StyleSheet, View } from "react-native";
+
+const videoSource = require("../assets/videos/FadeInBackground.mp4");
 
 export default function Index() {
-  return <View></View>;
+  const player = useVideoPlayer(videoSource, (player) => {
+    player.loop = true;
+    player.play();
+  });
+
+  return (
+    <View style={styles.container}>
+      <VideoView
+        style={styles.video}
+        player={player}
+        allowsPictureInPicture={false}
+        contentFit={"cover"}
+      />
+    </View>
+  );
 }
 
-/*     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View> */
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  video: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+  },
+});
